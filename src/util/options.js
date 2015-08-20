71308a0edf979d43131bc7bf450c69ca071ab384
Evan You <yyx990803@gmail.com>
2015-08-20 10:02:18 -0400

    also support PascalCased assets

--- src/util/options.js
+++ src/util/options.js
@@ -342,7 +342,9 @@ exports.mergeOptions = function merge (parent, child, vm) {
 exports.resolveAsset = function resolve (options, type, id) {
 
   var camelizedId = _.camelize(id)
-  var asset = options[type][id] || options[type][camelizedId]
+  var pascalizedId = camelizedId.charAt(0).toUpperCase() + camelizedId.slice(1)
+  var assets = options[type]
+  var asset = assets[id] || assets[camelizedId] || assets[pascalizedId]
 
   if (process.env.NODE_ENV !== 'production') {
     if (!asset && !config.strict) {
@@ -355,7 +357,8 @@ exports.resolveAsset = function resolve (options, type, id) {
     (!config.strict || options._repeat)
   ) {
     options = options._parent.$options
-    asset = options[type][id] || options[type][camelizedId]
+    assets = options[type]
+    asset = assets[id] || assets[camelizedId] || assets[pascalizedId]
   }
   return asset
 }
